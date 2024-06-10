7aa48a0d27013efad7dc8cabf204417f8f958d67
Cleman25 <clementan25@gmail.com>
2024-06-10 01:32:37 -0700

    Sidebar Component

---
+++ resources/views/components/sidebar-item.blade.php
@@ -0,0 +1,10 @@
+<div class="sidebar-item">
+    {{-- link component --}}
+    <x-link
+        class="sidebar-link"
+        type="{{ $type }}"
+        icon="{{ $icon }}"
+        page="{{ $route }}"
+        >
+    </x-link>
+</div>
