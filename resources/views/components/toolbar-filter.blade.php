53e07439c2ee8daa656dae04e269becc02cc1800
Cleman25 <clementan25@gmail.com>
2024-06-10 23:22:24 -0700

    Initial Toolbar and Pagination Components

---
+++ resources/views/components/toolbar-filter.blade.php
@@ -0,0 +1,14 @@
+<x-toolbar-item>
+    <select>
+        <option>Filter</option>
+        <option>Service Role</option>
+        <option>Computer Science</option>
+        <option>Management</option>
+        <option>Physics</option>
+        <option>Stats</option>
+    </select>
+    <div class="filter-order">
+        <button class="material-symbols-outlined">arrow_upward</button>
+        <button class="material-symbols-outlined">arrow_downward</button>
+    </div>
+</x-toolbar-item>
---
+++ resources/views/components/toolbar-search.blade.php
@@ -0,0 +1,7 @@
+<x-toolbar-item>
+    <form class="form" id="instructor-form">
+        <div class="form-item">
+            <input type="search" name="search" id="instructor_search" placeholder="Search...">
+        </div>
+    </form>
+</x-toolbar-item>
