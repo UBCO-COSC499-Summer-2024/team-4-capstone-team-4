8cd0b14162feba149615a02321d05342cb19955a
Kate <knaychuk@gmail.com>
2024-06-12 12:12:54 -0700

    added basic header to dashboard

--- resources/views/components/header/brand.blade.php
+++ resources/views/components/header/brand.blade.php
@@ -1,6 +1,6 @@
-<section id="header-brand">
+<section id="header-brand" class="flex items-center m-5 ">
     <div id="header-img">
-        <img src="{{ $brandImg }}" alt="{{ $brandAlt }}">
+        <img src="{{ $brandImg }}" alt="{{ $brandAlt }}" width="40" height="40">
     </div>
-    <h1 class="nos">{{ $brandName }}</h1>
+    <h1 class="nos text-2xl mx-4">{{ $brandName }}</h1>
 </section>
