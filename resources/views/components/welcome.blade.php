ebb16db2b284ad01669b109c8eb79517449babf1
Kate <knaychuk@gmail.com>
2024-06-12 12:06:06 -0700

    temporary dashboard template

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,1 +1,8 @@
-<div>Your Dashboard</div>
+{{-- temp --}}
+
+<div class="text-2xl mx-2 mb-4">Your Dashboard</div>
+<div class="flex">
+    <div class="p-20 text-2xl bg-white mx-2">Chart</div>
+    <div class="p-20 text-2xl bg-white mx-2">Chart</div>
+    <div class="p-20 text-2xl bg-white mx-2">Chart</div>
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,17 +2,16 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 
     <div class="">
-        <div class="">
-                {{-- header component --}}
-
+            {{-- header component --}}
+        <div class="flex m-5">
                 {{-- sidebar component --}}
-                <div class="bg-gray-200 min-w-80 rounded-lg p-10 max-w-[300px]">
-                    <x-sidebar :items="[]" />
-                </div>
-                {{-- future visualizations component --}}
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <x-welcome />
-                </div>
+            <div class="bg-gray-200 rounded-lg p-10 min-w-[300px] min-h-screen">
+                <x-sidebar :items="[]" />
+            </div>
+            {{-- future visualizations component --}}
+            <div class="mx-2">
+                <x-welcome />
+            </div>
         </div>
     </div>
 </x-app-layout>
