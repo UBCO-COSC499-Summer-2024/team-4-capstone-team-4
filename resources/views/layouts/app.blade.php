429632e56b18d6dd6876fc4c7e77adf6e17e9a6d
Kate <knaychuk@gmail.com>
2024-06-12 14:24:46 -0700

    added dashboard.css file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,12 +10,14 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Styles -->
         @livewireStyles
+        {{-- <link href="{{ asset('css/dashboard.css') }}" rel="stylesheet"> --}}
     </head>
     <body class="font-sans antialiased">
         <x-banner />
