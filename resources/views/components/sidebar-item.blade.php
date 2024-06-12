fddc203908eb5c8bfe6a449104a997f2d86b48b2
Kate <knaychuk@gmail.com>
2024-06-12 11:54:59 -0700

    sidebar component added to dashboard

--- resources/views/components/sidebar-item.blade.php
+++ resources/views/components/sidebar-item.blade.php
@@ -1,10 +1,14 @@
-<div class="sidebar-item">
+<div class="sidebar-item flex items-center">
     {{-- link component --}}
+
+    {{-- *note: changed $type to $title --}}
     <x-link
         class="sidebar-link"
-        type="{{ $type }}"
+        type="{{ $title }}" 
         icon="{{ $icon }}"
         page="{{ $route }}"
         >
     </x-link>
+    {{-- add name beside icon --}}
+    <a href="#" class="text-xl mx-4 my-4">{{$title}}</a>
 </div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,18 @@
 <x-app-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
-            </div>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+
+    <div class="">
+        <div class="">
+                {{-- header component --}}
+
+                {{-- sidebar component --}}
+                <div class="bg-gray-200 min-w-80 rounded-lg p-10 max-w-[300px]">
+                    <x-sidebar :items="[]" />
+                </div>
+                {{-- future visualizations component --}}
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <x-welcome />
+                </div>
         </div>
     </div>
 </x-app-layout>
