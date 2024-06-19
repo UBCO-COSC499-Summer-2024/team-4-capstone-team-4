3e94013ef46a66e4714ca0cb80669653409f2618
Kate <knaychuk@gmail.com>
2024-06-18 18:22:31 -0700

    drag and drop

--- resources/views/components/import-form.blade.php
+++ resources/views/components/import-form.blade.php
@@ -7,35 +7,65 @@
                 <label for="data">Choose a type:</label>
 
                 <select name="data" id="data">
-                    <option value="volvo">SEI Data</option>
+                    <option value="sei">SEI Data</option>
                     <option value="saab">Workday Data</option>
                 </select> 
             </div>
 
             <div class="import-file">
                 <div>Upload a File:</div>
-                <div class="import-file-area">
-                    <h3>Choose a file or drag & drop it here</h3>
-                    <label class="import-file-input"> Browse Files
-                    <input type="file" name="file" id="file" onchange="updateFileName()" />
-                    </label>
-                    <span id="file-name"></span>
-                </div>
+                <div class="drop-zone">
+                    <span class="drop-zone-prompt">Drop file here or click to upload</span>
+                    <input type="file" name="myFile" class="drop-zone-input">
+                  </div>
+                  <span id="file-name" class="drop-zone-file-name"></span>
             </div>
 
             <button   button type="submit" class="btn btn-primary mt-3">Upload</button>
         </form>
-    </div>
-
-
-
-    <x-import-modal />
+    </div> 
 </div>
 
 <script>
-    function updateFileName() {
-        var input = document.getElementById('file');
-        var fileName = input.files[0].name;
-        document.getElementById('file-name').textContent = fileName;
+    document.querySelectorAll(".drop-zone-input").forEach((inputElement) => {
+        const dropZoneElement = inputElement.closest(".drop-zone");
+
+        dropZoneElement.addEventListener("click", (e) => {
+            inputElement.click();
+        });
+
+        inputElement.addEventListener("change", (e) => {
+            if (inputElement.files.length) {
+                updateFileName(dropZoneElement, inputElement.files[0]);
+            }
+        });
+
+        dropZoneElement.addEventListener("dragover", (e) => {
+            e.preventDefault();
+            dropZoneElement.classList.add("drop-zone-over");
+        });
+
+        ["dragleave", "dragend"].forEach((type) => {
+            dropZoneElement.addEventListener(type, (e) => {
+                dropZoneElement.classList.remove("drop-zone-over");
+            });
+        });
+
+        dropZoneElement.addEventListener("drop", (e) => {
+            e.preventDefault();
+
+            if (e.dataTransfer.files.length) {
+                inputElement.files = e.dataTransfer.files;
+                updateFileName(dropZoneElement, e.dataTransfer.files[0]);
+            }
+
+            dropZoneElement.classList.remove("drop-zone-over");
+        });
+    });
+
+    function updateFileName(dropZoneElement, file) {
+        const fileNameElement = document.getElementById("file-name");
+        fileNameElement.textContent = `Selected file: ${file.name}`;
     }
 </script>
+
--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -2,5 +2,6 @@
   <div class="content container mt-5">
     <h1>{{ __('Import Data') }}</h1>
     <x-import-form />
+    <x-import-modal />
 </div>
 </x-app-layout>
