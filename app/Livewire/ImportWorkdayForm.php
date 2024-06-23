90ff35f01937ff429f6549e0a6f813817c157b2a
Kate <knaychuk@gmail.com>
2024-06-23 00:14:55 -0700

    add to sei table

--- app/Livewire/ImportWorkdayForm.php
+++ app/Livewire/ImportWorkdayForm.php
@@ -2,27 +2,40 @@
 
 namespace App\Livewire;
 
+use App\Models\SeiData;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class ImportWorkdayForm extends Component
 {
-
-    public $id = '';
-
-    public $firstname = '';
-
-    public $lastname = '';
-
+    public $testCid = 123456;
     public $rows = [];
 
     public function mount() {
         $this->rows = [
-            ['id' => $this->id, 'firstname' => $this->firstname, 'lastname' => $this->lastname],
+            ['cid' => '', 'q1' => '', 'q2' => '', 'q3' => '', 'q4' => '', 'q5' => '', 'q6' => ''],
         ];
     }
 
+    public function rules() {
+        $rules = [];
+
+     
+        foreach ($this->rows as $index => $row) {
+            $rules["rows.{$index}.cid"] = 'required|min:1';
+            $rules["rows.{$index}.q1"] = '';
+            $rules["rows.{$index}.q2"] = '';
+            $rules["rows.{$index}.q3"] = '';
+            $rules["rows.{$index}.q4"] = '';
+            $rules["rows.{$index}.q5"] = '';
+            $rules["rows.{$index}.q6"] = '';
+        }
+
+        return $rules;
+    }
+
     public function addRow() {
-        $this->rows[] = ['id' => $this->id, 'firstname' => $this->firstname, 'lastname' => $this->lastname];
+        $this->rows[] =  ['cid' => '', 'q1' => '', 'q2' => '', 'q3' => '', 'q4' => '', 'q5' => '', 'q6' => ''];
     }
 
     public function deleteRow($row) {
@@ -31,6 +44,33 @@ class ImportWorkdayForm extends Component
     }
 
     public function handleClick() {
+
+        // dd($this->rows);
+        $this->validate();
+    
+        
+        foreach ($this->rows as $row) {
+            // dd($row);
+
+            SeiData::create([
+                'course_section_id' => $row['cid'],
+                // 'questions' => json_encode([
+                //     'q1' => $row['q1'],
+                //     'q2' => $row['q2'],
+                //     'q3' => $row['q3'],
+                //     'q4' => $row['q4'],
+                //     'q5' => $row['q5'],
+                //     'q6' => $row['q6'],
+                // ]),
+            ]);
+
+        }
+
+        $this->rows = [
+            ['cid' => '', 'q1' => '', 'q2' => '', 'q3' => '', 'q4' => '', 'q5' => '', 'q6' => ''],
+        ];
+
+        session()->flash('success', 'Successfully Created!');
 
     }
 
--- resources/views/livewire/import-workday-form.blade.php
+++ resources/views/livewire/import-workday-form.blade.php
@@ -16,25 +16,32 @@
         <div class="import-form-row flex justify-between items-center p-2 border-b">
             <div class="w-1/12 pl-2">{{ $index + 1 }}</div>
             <div class="import-input w-4/12">
-                <input type="text" placeholder="123456" wire:model="rows.{{$index}}.id" class="p-1 w-full">
+                <input type="text" placeholder="123456" wire:model="rows.{{$index}}.cid" class="p-1 w-full">
+                @error('rows.'.$index.'.cid')<span>{{ $message }}</span>@enderror
             </div>
             <div class="import-input w-3/12">
-                <input type="text" placeholder="#" wire:model="rows.{{$index}}.firstname" class="p-1 w-full">
+                <input type="text" placeholder="#" wire:model="rows.{{$index}}.q1" class="p-1 w-full">
+                @error('rows.'.$index.'.q1')<span>{{ $message }}</span>@enderror
             </div>
             <div class="import-input w-3/12">
-                <input type="text" placeholder="#" wire:model="rows.{{$index}}.lastname" class="p-1 w-full">
+                <input type="text" placeholder="#" wire:model="rows.{{$index}}.q2" class="p-1 w-full">
+                @error('rows.'.$index.'.q2')<span>{{ $message }}</span>@enderror
             </div>
             <div class="import-input w-3/12">
-                <input type="text" placeholder="#" wire:model="rows.{{$index}}.lastname" class="p-1 w-full">
+                <input type="text" placeholder="#" wire:model="rows.{{$index}}.q3" class="p-1 w-full">
+                @error('rows.'.$index.'.q3')<span>{{ $message }}</span>@enderror
             </div>
             <div class="import-input w-3/12">
-                <input type="text" placeholder="#" wire:model="rows.{{$index}}.lastname" class="p-1 w-full">
+                <input type="text" placeholder="#" wire:model="rows.{{$index}}.q4" class="p-1 w-full">
+                @error('rows.'.$index.'.q4')<span>{{ $message }}</span>@enderror
             </div>
             <div class="import-input w-3/12">
-                <input type="text" placeholder="#" wire:model="rows.{{$index}}.lastname" class="p-1 w-full">
+                <input type="text" placeholder="#" wire:model="rows.{{$index}}.q5" class="p-1 w-full">
+                @error('rows.'.$index.'.q5')<span>{{ $message }}</span>@enderror
             </div>
             <div class="import-input w-3/12">
-                <input type="text" placeholder="#" wire:model="rows.{{$index}}.lastname" class="p-1 w-full">
+                <input type="text" placeholder="#" wire:model="rows.{{$index}}.q6" class="p-1 w-full">
+                @error('rows.'.$index.'.q6')<span>{{ $message }}</span>@enderror
             </div>
 
             <div class="w-3/12 flex justify-center border-l border-gray-300">
@@ -43,8 +50,10 @@
                 </button>
             </div>
         </div>    
+
         @endforeach
 
+       
         <div class="mt-4 flex justify-end space-x-2">
             <button type="button" wire:click="addRow" class="bg-blue-500 text-black p-2 rounded hover:bg-blue-600">Add Row</button>
             <button type="submit" class="bg-green-500 text-black p-2 rounded hover:bg-green-600">Save</button>
