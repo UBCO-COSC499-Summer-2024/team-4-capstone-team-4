6ffbb37f5eeebb70fd887950ade2317c094113e5
Sumer26 <sumer1@student.ubc.ca>
2024-07-04 23:41:19 -0700

    Saving changes

--- resources/views/components/save-confirm.blade.php
+++ resources/views/components/save-confirm.blade.php
@@ -1,4 +1,4 @@
-<div id="confirmationModal" class="modal" tabindex="-1" role="dialog">
+{{-- <div id="confirmationModal" class="modal" tabindex="-1" role="dialog">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -19,4 +19,4 @@
 </div>
 
 
-
+ --}}
