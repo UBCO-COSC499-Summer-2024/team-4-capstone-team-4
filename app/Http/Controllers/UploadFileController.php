eb6316ff242d555e48592431b29db949ff8115fc
Kate <knaychuk@gmail.com>
2024-07-16 14:55:22 -0700

    trim dataa

--- app/Http/Controllers/UploadFileController.php
+++ app/Http/Controllers/UploadFileController.php
@@ -12,9 +12,32 @@ class UploadFileController extends Controller {
     private function readCSV($filePath)
     {
         $csvData = [];
+        $result = '';
         if (($handle = fopen($filePath, 'r')) !== false) {
             $firstLine = fgets($handle);
 
+            $firstHyphenPos = strpos($firstLine, '-');
+
+            // Find the position of the second hyphen
+                $secondHyphenPos = strpos($firstLine, '-', $firstHyphenPos + 1);
+
+                // Extract the substring up to the second hyphen
+                if ($secondHyphenPos !== false) {
+                    $result = substr($firstLine, 0, $secondHyphenPos);
+                    echo $result;
+                } else {
+                    echo "Second hyphen not found.";
+                }
+
+            if ($result) {
+                // Use a regular expression to extract the area, number, and section
+                if (preg_match('/^([A-Z]{4})_([A-Z])\s(\d{3})-(\d{3})/', trim($result), $matches)) {
+                    $csvData['Area'] = $matches[1];
+                    $csvData['Number'] = $matches[3];
+                    $csvData['Section'] = $matches[4];
+                }
+                $csvData['Course Name'] = trim($firstLine);
+            }
 
             while (($row = fgetcsv($handle, 1000, ',')) !== false) {
                 // Ensure each row has at least two elements to form a key-value pair
@@ -25,56 +48,81 @@ class UploadFileController extends Controller {
                 }
             }
             fclose($handle);
-        }
 
-        if ($firstLine) {
-            $csvData['Course Name'] = $firstLine;
+            if(array_key_exists('Academic Period', $csvData)) {
+                if($csvData["Academic Period"]) {
+         
+                    if(preg_match('/^(\d{4}) (Summer|Winter)/' , $csvData["Academic Period"], $matches)) {
+                        $csvData['Year'] = $matches[1];
+                        if($matches[2] == 'Winter') {
+                            $csvData['Session'] = 'W';
+                        } else if($matches[2] == 'Summer') {
+                            $csvData['Session'] = 'S';
+                        }
+                    }
+                }
+            }
+            
+            if(array_key_exists("Enrolled/Capacity", $csvData)) {
+                if($csvData["Enrolled/Capacity"]) {
+                    if(preg_match('/^(\d+)\//', $csvData["Enrolled/Capacity"], $matches)) {
+                        $csvData['Enrolled'] = $matches[1];
+                    }
+                }
+    
+            }
+            
+            // dd($csvData);
         }
 
         return $csvData;
     }
 
     public function upload(Request $request) {
-        $trimCSV = [];
-                // Validate the request
+        $finalCSVs = [];
+        $uploadedFiles = [];
+     
         $request->validate([
             'files.*' => 'required|file|mimes:csv,txt|max:2048',
         ]);
 
-        $uploadedFiles = [];
-
         foreach ($request->file('files') as $file) {
             $filePath = $file->getRealPath();
             $csvData = $this->readCSV($filePath);
-    
+        
             $uploadedFiles[] = [
                 'fileName' => $file->getClientOriginalName(),
                 'csvData' => $csvData,
             ];
         }
 
-        dd($uploadedFiles);
-
-        foreach ($csvData as $key => $value) {
-            switch ($key) {
-                case 'Course Name':
-                    $trimCSV[$key] = $value;
-                    break;
-                case 'Academic Period':
-                    $trimCSV[$key] = $value;
-                    break;
-                case 'Enrolled/Capacity':
-                    $trimCSV[$key] = $value;
-                    break;
+        foreach ($uploadedFiles as $uploadedFile) {
+            $trimCSV = [];
+            foreach ($uploadedFile['csvData'] as $key => $value) {
+                switch ($key) {
+                    case 'Area':
+                    case 'Number':
+                    case 'Section':
+                    case 'Year':
+                    case 'Session':
+                    case 'Enrolled':
+                        $trimCSV[$key] = $value;
+                        break;
+                }
             }
+            $finalCSVs[] = $trimCSV;
         }
-    
 
         // dd($trimCSV);
+    
+
+        dd($finalCSVs);
 
         session()->flash('message', 'File uploaded successfully!');
         session()->put('csvData', $csvData);
         session()->put('trimCSV', $trimCSV);
+        session()->put('finalCSVs', $finalCSVs);
+
 
         // Process the file as needed
         // For demonstration, just dump the file path
--- resources/views/components/upload-file.blade.php
+++ resources/views/components/upload-file.blade.php
@@ -1,6 +1,7 @@
 @php
     // $csvData = session('csvData');
-    $trimCSV = session('trimCSV');
+    $trimCSV = session('trimCSV', []);
+    $finalCSVs = session('finalCSVs', []);
 @endphp
 
 
@@ -21,86 +22,12 @@
     <div>{{$value}}</div>
 @endforeach --}}
 
-@foreach ($trimCSV as $key => $value)
+{{-- @foreach ($trimCSV as $key => $value)
     <div>{{$key}} : {{$value}}</div>
+@endforeach --}}
+
+@foreach ($finalCSVs as $finalCSV)
+    @foreach ($finalCSV as $key => $value)
+    <div>{{$key}}{{$value}}</div>
+    @endforeach
 @endforeach
-
-
-{{-- <div class="import-container">
-    <div class="import-form-container">    
-        <form action="{{ route('import') }}" method="POST" enctype="multipart/form-data">
-       
-        @csrf
-            <div class="import-select">
-                <label class="import-label" for="data">Choose a type:</label>
-
-                <select name="data" id="data">
-                    <option value="sei">SEI Data</option>
-                    <option value="saab">Workday Data</option>
-                </select> 
-            </div>
-
-            <div class="import-file">
-                <label class="import-label">Upload a File:</label>
-                <div class="drop-zone">
-                    <span class="drop-zone-prompt">Drop file here or <div class="drop-zone-button">Browse</div></span>
-                    <input type="file" name="file" class="drop-zone-input outline outline-blue-500">
-                  </div>
-                 
-            </div>
-            <span id="file-name" class="drop-zone-file-name"></span>
-            <button type="submit" class="btn btn-primary mt-3">Upload</button>
-        </form>
-
-        @if ($errors->any())
-        <div>
-            <x-validation-errors />
-        </div>  
-        @endif
-    </div> 
-
-
-</div>
-
-<script>
-    document.querySelectorAll(".drop-zone-input").forEach((inputElement) => {
-        const dropZoneElement = inputElement.closest(".drop-zone");
-
-        dropZoneElement.addEventListener("click", (e) => {
-            inputElement.click();
-        });
-
-        inputElement.addEventListener("change", (e) => {
-            if (inputElement.files.length) {
-                updateFileName(dropZoneElement, inputElement.files[0]);
-            }
-        });
-
-        dropZoneElement.addEventListener("dragover", (e) => {
-            e.preventDefault();
-            dropZoneElement.classList.add("drop-zone-hover");
-        });
-
-        ["dragleave", "dragend"].forEach((type) => {
-            dropZoneElement.addEventListener(type, (e) => {
-                dropZoneElement.classList.remove("drop-zone-hover");
-            });
-        });
-
-        dropZoneElement.addEventListener("drop", (e) => {
-            e.preventDefault();
-
-            if (e.dataTransfer.files.length) {
-                inputElement.files = e.dataTransfer.files;
-                updateFileName(dropZoneElement, e.dataTransfer.files[0]);
-            }
-
-            dropZoneElement.classList.remove("drop-zone-hover");
-        });
-    });
-
-    function updateFileName(dropZoneElement, file) {
-        const fileNameElement = document.getElementById("file-name");
-        fileNameElement.textContent = `Selected file: ${file.name}`;
-    }
-</script> --}}
