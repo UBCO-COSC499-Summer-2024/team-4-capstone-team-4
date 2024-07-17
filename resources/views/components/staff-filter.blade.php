a6b6ed82fa44717b10955a538068e957e0496169
cmagoke <113209594+cmagoke@users.noreply.github.com>
2024-07-17 15:11:52 -0700

    changed green

--- resources/views/components/staff-filter.blade.php
+++ resources/views/components/staff-filter.blade.php
@@ -1,7 +1,7 @@
 <div class="flex items-center justify-center p-4 relative">
     <button id="filterButton" data-dropdown-toggle="dropdown" class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
         <span class="sr-only">Filter</span>
-        Area
+        Filter by Area
         <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
         </svg>
