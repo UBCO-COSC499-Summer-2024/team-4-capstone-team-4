4768fb7267c265760d1f4e30616356bad910d1b0
cmagoke <113209594+cmagoke@users.noreply.github.com>
2024-07-17 14:06:13 -0700

    fixed button issue

--- resources/views/components/staff-button-red.blade.php
+++ resources/views/components/staff-button-red.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['class' => 'inline-flex items-center px-3 py-1.5 text-[#ea3030] hover:text-white border border-[#ea3030] hover:bg-[#ea3030] focus:ring-1 focus:outline-none focus:ring-[#ea3030] font-medium rounded-lg text-sm text-center me-1 mb-2']) }}>
+<button {{ $attributes->merge(['class' => 'inline-flex items-center px-3 py-1.5 text-[#ea3030] hover:text-white border border-[#ea3030] hover:bg-[#ea3030] focus:ring-1 focus:outline-none focus:ring-[#ea3030] font-medium rounded-lg text-sm text-center']) }}>
     {{ $slot }}
 </button>
