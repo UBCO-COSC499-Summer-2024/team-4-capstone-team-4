9c9b4626ee968b182c8269843a92dfa56ce5c80c
Sumer26 <sumer1@student.ubc.ca>
2024-07-21 16:47:39 -0700

    Assign, Save, Cancel, Edit buttons visibility improved

--- resources/views/components/assign-button.blade.php
+++ resources/views/components/assign-button.blade.php
@@ -1,5 +1,5 @@
 <button id="assignButton" type="button" 
-   class="custom-ubc-blue hover:text-white border focus:ring-1 focus:outline-none font-medium rounded-lg text-sm px-5 py-2 text-center me-1 mb-2" 
+class="custom-ubc-blue hover:text-white focus:ring-1 focus:outline-none font-bold rounded-lg text-sm px-5 py-2 text-center me-1 mb-2" 
    style="display: block;" data-toggle="modal" data-target="#assignModal">
    Assign
 </button>
--- resources/views/components/cancel-button.blade.php
+++ resources/views/components/cancel-button.blade.php
@@ -1,5 +1,3 @@
-<button id="cancelButton" type="button" class="text-red-500 hover:text-white 
-border border-red-500 hover:bg-red-500 focus:ring-1 focus:outline-none focus:ring-red-300 
-font-medium rounded-lg text-sm px-5 py-2 text-center me-1 mb-2" style="display: none;">
-Cancel
+<button id="cancelButton" type="button" class="custom-red hover:text-white focus:ring-1 focus:outline-none font-bold rounded-lg text-sm px-5 py-2 text-center me-1 mb-2"  style="display: none;">
+    Cancel
 </button>
