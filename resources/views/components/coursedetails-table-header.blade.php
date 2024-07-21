617603e494c55662172d39b8969933ce27226e08
Sumer26 <sumer1@student.ubc.ca>
2024-07-21 11:09:51 -0700

    Table Headers Simplified

--- resources/views/components/coursedetails-table-header.blade.php
+++ resources/views/components/coursedetails-table-header.blade.php
@@ -1,6 +1,6 @@
 <thead class="sticky-header">
     <tr class="svcr-list-header">
-        @foreach(['courseNames' => 'Course Name', 'departmentName' => 'Department Name', 'enrolledStudents' => 'Enrolled Students', 'droppedStudents' => 'Dropped Students', 'courseCapacity' => 'Course Capacity', 'seiData' => 'SEI Data'] as $field => $label)
+        @foreach(['courseNames' => 'Course Name', 'departmentName' => 'Department Name', 'enrolledStudents' => 'Enrolled', 'droppedStudents' => 'Dropped', 'courseCapacity' => 'Course Capacity', 'seiData' => 'SEI Data'] as $field => $label)
             <th scope="col" class="sortable {{$sortField === $field ? ($sortDirection === 'asc' ? 'th-sort-asc' : 'th-sort-desc') : ''}} p-8 text-left text-xs font-large text-white uppercase tracking-wider svcr-list-header-item" style="padding: 0.5rem;">
                 <div class="flex items-center">
                     <span>{{$label}}</span>
