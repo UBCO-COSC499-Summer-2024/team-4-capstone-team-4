08f27ba4780eec70dab9920d4605a7006217c296
cmagoke <113209594+cmagoke@users.noreply.github.com>
2024-07-23 13:02:53 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/scrollbar.css',
+                'resources/css/form.css',
                 'resources/js/app.js',
             ],
             refresh: true,
