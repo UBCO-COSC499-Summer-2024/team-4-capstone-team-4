c7e2e4dabcd993d3810b20ff64e485b97394a78e
aaan-gelina <blackhammer971@Leif-II>
2024-07-23 18:01:51 -0700

    Fix button frontend, add profile container

--- resources/views/performance.blade.php
+++ resources/views/performance.blade.php
@@ -1,6 +1,16 @@
+@vite(['resources/css/performance.css'])
+
 <x-app-layout>
     <div class="content">
         <h1>{{ $name }}'s Dashboard</h1>
+            <div class="button-container">
+                <x-performance-export-button :id="$performance->instructor_id">
+                    View Report
+                </x-performance-export-button>
+            </div>
+            <div class="profile-container">
+                Profile Coming Soon...
+            </div>
             @if ($hasTarget)
                 <section class="dash-top">
                     <x-instructor-target :chart1="$chart1" :chart4="$chart4" :currentMonth="$currentMonth" :ranking="$ranking" :performance="$performance" />
