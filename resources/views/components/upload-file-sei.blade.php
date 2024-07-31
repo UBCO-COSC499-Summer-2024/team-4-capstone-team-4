1985a4bfb17cf5d7860aeb6f198efba17822c7d5
Kate <knaychuk@gmail.com>
2024-07-31 00:56:59 -0700

    add nav buttons to other file upload pages

--- resources/views/components/upload-file-sei.blade.php
+++ resources/views/components/upload-file-sei.blade.php
@@ -1,6 +1,15 @@
-{{-- @php
+@php
     $finalCSVs = session('finalCSVs', []);
-@endphp --}}
+@endphp
+
+<div class="absolute left-0 top-10">
+    <button class="bg-white text-[#3b4779] border border-[#3b4779] px-3 py-2 mx-2 rounded-lg hover:bg-[#3b4779] hover:text-white" 
+        onclick="location.href='{{ route('upload.file.show.workday') }}'">
+        <span class="material-symbols-outlined">arrow_back</span>
+        Upload File (Workday) Instead
+    </button>
+</div>
+
 
 <livewire:drag-and-drop route="upload.file.sei"/>
 
--- resources/views/components/upload-file-workday.blade.php
+++ resources/views/components/upload-file-workday.blade.php
@@ -2,6 +2,14 @@
     $finalCSVs = session('finalCSVs', []);
 @endphp
 
+<div class="absolute left-0 top-10">
+    <button class="bg-white text-[#3b4779] border border-[#3b4779] px-3 py-2 mx-2 rounded-lg hover:bg-[#3b4779] hover:text-white" 
+        onclick="location.href='{{ route('upload.file.show.sei') }}'">
+        <span class="material-symbols-outlined">arrow_back</span>
+        Upload File (SEI) Instead
+    </button>
+</div>
+
 <livewire:drag-and-drop route="upload.file.workday"/>
 
 @if (session('finalCSVs'))
