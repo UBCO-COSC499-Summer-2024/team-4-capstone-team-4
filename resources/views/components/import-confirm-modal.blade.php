89de472b89f2240dc4c91617ba80401bc7dd81ef
Kate <knaychuk@gmail.com>
2024-08-01 16:55:49 -0700

    military time validation + change confirm modal wording

--- resources/views/components/import-confirm-modal.blade.php
+++ resources/views/components/import-confirm-modal.blade.php
@@ -5,12 +5,14 @@
         </div>
     </div>
     <div class="p-6 mt-2 mb-10 text-center">   
-        <p class="text-2xl font-bold mb-6">The Following Courses Already Exist and Will be Updated if Confirmed</p>
+        <p class="text-2xl font-bold mb-6">The Following Courses Already Exist and Will be Updated/Overwritten if Saved</p>
+        <div class="max-h-60 overflow-y-auto">
         @if($duplicateCourses)
             @foreach($duplicateCourses as $course)
                 <p class="text-xl">{{ $course->prefix }} {{$course->number}} {{ $course->section }} - {{ $course->year }}{{ $course->session }}{{ $course->term }}</p>
             @endforeach
         @endif
+        </div>
         <div class="flex justify-center mt-6">
             <button class="bg-white text-red-500 border border-red-500 py-2 px-4 mx-2 rounded-lg hover:bg-red-500 hover:text-white" 
                     wire:click="closeConfirmModal">
@@ -18,7 +20,7 @@
             </button>
             <button class="bg-white text-[#3D8B57] border border-[#3D8B57] py-2 px-4 mx-2 rounded-lg hover:bg-[#3D8B57] hover:text-white" 
                     wire:click="userConfirmDuplicate">
-                Confirm Update
+                I understand
             </button>
             
         </div>
