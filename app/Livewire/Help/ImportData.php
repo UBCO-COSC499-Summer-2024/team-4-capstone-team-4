48e811322817c2915e51f1a354fcd443f0f7c637
Kate <knaychuk@gmail.com>
2024-08-01 17:37:55 -0700

    create import data help template

---
+++ app/Livewire/Help/ImportData.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Help;
+
+use Livewire\Component;
+
+class ImportData extends Component
+{
+    public function render()
+    {
+        return view('livewire.help.import-data');
+    }
+}
