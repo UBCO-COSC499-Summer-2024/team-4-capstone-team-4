1f32977ba1ea98d4dfda122c1ac606ce8682d7c9
cmagoke <113209594+cmagoke@users.noreply.github.com>
2024-08-02 21:37:08 -0700

    made icons bigger, changed dropdown to button for targets

--- resources/views/livewire/staff-list.blade.php
+++ resources/views/livewire/staff-list.blade.php
@@ -28,7 +28,8 @@
                     <x-staff-button-red wire:click="exit" id="staff-exit" name="staff-exit">Cancel</x-staff-button-red>
                 @else
                     <x-staff-filter />
-                    <x-staff-dropdown :selectedYear="$selectedYear" :selectedMonth="$selectedMonth"/>
+                    <x-staff-button id="add-target-hours" wire:click="showTargetModal">Add Targets</x-staff-button>
+                    <x-staff-button id="edit-target-hours" wire:click="$set('editMode', true)">Edit Targets</x-staff-button>
                 @endif
             @endif
         </div>
