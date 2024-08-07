6039a31aa1135813309c6577356fa08aa149e250
Kate <knaychuk@gmail.com>
2024-08-07 13:02:15 -0700

    change route

--- resources/views/components/upload-file-sei.blade.php
+++ resources/views/components/upload-file-sei.blade.php
@@ -4,9 +4,9 @@
 
 <div class="absolute left-0 top-10">
     <button class="bg-white text-[#3b4779] border border-[#3b4779] px-3 py-2 mx-2 rounded-lg hover:bg-[#3b4779] hover:text-white" 
-        onclick="location.href='{{ route('upload.file.show.workday') }}'">
+        onclick="location.href='{{ route('import') }}'">
         <span class="material-symbols-outlined">arrow_back</span>
-        Upload File (Workday) Instead
+        Back to Manual Import
     </button>
 </div>
 
