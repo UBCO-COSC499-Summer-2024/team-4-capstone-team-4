5bdfa5d11f130a874b3600d12fdb7a43b655faf1
Kate <knaychuk@gmail.com>
2024-08-06 19:59:22 -0700

    fix routing

--- resources/views/livewire/upload-file-form-assign-tas.blade.php
+++ resources/views/livewire/upload-file-form-assign-tas.blade.php
@@ -105,7 +105,7 @@
     @if(session('success'))
         @if($showModal) 
             <div class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-                @include('components.import-modal', ['user' => $user, 'moreText' => 'Assign More'])
+                @include('components.import-modal', ['user' => $user, 'moreText' => 'Upload Another File'])
             </div>
         @endif
     @endif
